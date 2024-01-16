0e04bc993b0e40b39804234c000d39a4a819416c
ksmith92272 <152031348+ksmith92272@users.noreply.github.com>
2024-01-16 14:11:30 -0600

    Added Typewriter JS plugin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 function fetchJoke() {
+	let jokeElement = document.getElementById("joke");
+	jokeElement.innerHTML = `Generating a joke for you.<br />Please wait...`;
+
 	let apiKey = `6atoab0f92eca3d102a54e7250f4dd0f`;
-	let apiContext = `You are an optimist, polite and helpful AI assistant with a great sense of humor. Please behave.`;
-	let apiPrompt = `Please tell me a hilarious dad joke`;
+	let apiContext = `You are an optimistic, polite and helpful AI assistant with a great sense of humor. Your response should be concise and should only be the joke. Do not inlcude a lead in. Include emojis. Please behave.`;
+	let apiPrompt = `Please tell me a funny joke about dogs`;
 
 	let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${apiPrompt}&context=${apiContext}&key=${apiKey}`;
 
@@ -11,7 +14,12 @@ function fetchJoke() {
 function showJoke(response) {
 	let jokeElement = document.getElementById("joke");
 	let joke = response.data.answer;
-	jokeElement.innerHTML = `${joke}`;
+	new Typewriter(jokeElement, {
+		strings: `${joke}`,
+		autoStart: true,
+		cursor: ``,
+		delay: 75,
+	});
 }
 
 let buttonElement = document.getElementById("joke-button");
