3cb5f35021d313ba24be3c9265e75195cc3712d0
ksmith92272 <152031348+ksmith92272@users.noreply.github.com>
2024-01-16 12:03:51 -0600

    Added Axios and api function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Hello");
+function showJoke(response) {
+	let jokeElement = document.getElementById("joke");
+	let joke = response.data.answer;
+	jokeElement.innerHTML = `${joke}`;
+}
+
+let apiKey = `6atoab0f92eca3d102a54e7250f4dd0f`;
+let apiContext = `You are a polite and helpful AI assistant. Your answer should be clear and consise; no longer than a sentence. Please include the years in service.`;
+let apiPrompt = `Who was the first female president in the world?`;
+
+let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${apiPrompt}&context=${apiContext}&key=${apiKey}`;
+
+axios.get(apiURL).then(showAnswer);
